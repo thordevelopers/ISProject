a0bcf4f0632a64050578c75b8c8c794059192c74
Alfredo Rivera <arivera87@uabc.edu.mx>
2020-03-05 09:40:13 -0800

    Initial commit with the controller and the view of login

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using ISProject.Models;
 namespace ISProject.Controllers
 {
     public class LoginController : Controller
@@ -17,6 +17,36 @@ namespace ISProject.Controllers
         public ActionResult Login()
         {
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult Login(UserCLS user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(user);
+            }
+            else
+            {
+                USERS user_db;
+                using (var db= new DB_PAAD_IAADEntities())
+                {
+                    user_db = db.USERS.Where(p => p.EMAIL==user.Email && p.PASSWORD==user.Password).FirstOrDefault();
+                }
+                if(user_db==null)
+                {
+                    return View(user);
+                }
+                else
+                {
+                    System.Diagnostics.Debug.WriteLine(user_db.ID);
+                    Session["id"] = user_db.ID;
+                    user = null;
+                    return RedirectToAction("Index", "PAAD");
+                }
+                
+            }
+            
         }
     }
 }
---
+++ Controllers/PAADController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ISProject.Models;
+
+namespace ISProject.Controllers
+{
+    public class PAADController : Controller
+    {
+        // GET: PAAD
+        public ActionResult Index()
+        {
+            System.Diagnostics.Debug.WriteLine("sesion.id: "+Session["id"]);
+            UserCLS id = new UserCLS();
+            id.Id =(int) Session["id"];
+            return View(id);
+        }
+    }
+}
