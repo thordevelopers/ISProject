25748e0f08331ba3bf52a1e3378fdb837608a4c8
Alfredo Rivera <arivera87@uabc.edu.mx>
2020-03-04 23:58:31 -0800

    Added model of users

---
+++ Models/UserCLS.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ISProject.Models
+{
+    public class UserCLS
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int Rol { get; set; }
+    }
+}
