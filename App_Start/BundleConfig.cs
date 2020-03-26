228d126675b658a7ca18e935bfa3da15a582f884
Alfredo Rivera <arivera87@uabc.edu.mx>
2020-03-26 16:39:28 -0700

    Modified _layaout, applyed the design of the navbar

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace ISProject
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Site.css"));
         }
     }
 }
