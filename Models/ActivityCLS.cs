5b1e50e77d3822c7f22a086819a3dda64c32989d
Alfonso Manjarrez Guido <poncho_997@uabc.edu.mx>
2020-03-31 22:36:58 -0700

    Model for the activity implemented

---
+++ Models/ActivityCLS.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ISProject.Models
+{
+    public class ActivityCLS
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(150, ErrorMessage = "Maximo 150 caracteres")]
+        public string actividad { get; set; }
+        [Required]
+        [StringLength(300, ErrorMessage = "Maximo 300 caracteres")]
+        public string produccion { get; set; }
+        [Required]
+        [StringLength(150, ErrorMessage = "Maximo 150 caracteres")]
+        public string lugar { get; set; }
+        [Required]
+        public int porcentaje_inicial { get; set; }
+        [Required]
+        public int porcentaje_final { get; set; }
+        [Required]
+        public bool cacei { get; set; }
+        [Required]
+        public bool cuerpo_academico { get; set; }
+        [Required]
+        public bool iso { get; set; }
+        public int id_paad { get; set; }
+        public int id_iad { get; set; }
+    }
+}
