26a44bf8f2246a7a278a9f37fc6b9e3b02bfd53f
Alfonso Manjarrez Guido <poncho_997@uabc.edu.mx>
2020-04-25 22:06:33 -0700

    View and Controller implemented, I used the PAADs DB_PAAD_IAD model, so didn't need to do a new Model. View is made without design, will work on it.

---
+++ Controllers/vistaPAADsController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ISProject.Models;
+
+
+namespace ISProject.Controllers
+{
+    public class vistaPAADsController : Controller
+    {
+        // GET: vistaPAADs
+        public ActionResult Index()
+        {
+            if (Session["user"] != null)
+            {
+                userPAADs(((Docentes)Session["user"]).id_docentes);
+                return View("~/Views/PAAD/vistaPAADs.cshtml");
+            }
+            else
+            {
+                return View("~/Views/Login/Login");
+            }
+        }
+
+        [HttpGet]
+        public ActionResult userPAADs(int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            else
+            {
+                List<PAADs> _paads;
+                using (var db = new DB_PAAD_IADEntities())
+                {
+                    _paads = db.PAADs.Where(p => p.docente == id).ToList();
+                }
+                if (_paads.Count == 0)
+                {
+                    return View();
+                }
+                else
+                {
+                    Session["user_paads"] = _paads;
+                }
+                _paads = null;
+            }
+            return View("~/Views/PAAD/vistaPAADs.cshtml");
+        }
+    }
+}
