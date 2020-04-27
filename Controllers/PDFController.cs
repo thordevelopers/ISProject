b726e9ca2b759aed310c812d7c485a3c61b22673
Alfredo Rivera <alfredo.rvlpz@gmail.com>
2020-04-27 13:41:45 -0700

    added rotative to controll pdf views

---
+++ Controllers/PDFController.cs
@@ -0,0 +1,26 @@
+﻿using Rotativa;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ISProject.Controllers
+{
+    public class PDFController : Controller
+    {
+        // GET: PDF
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        public ActionResult ToPDF()
+        {
+            return new ViewAsPdf("ToPDF") {
+                PageOrientation = Rotativa.Options.Orientation.Landscape
+            };
+        }
+    }
+}
