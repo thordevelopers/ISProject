e11234ec4ee11ca2bc0e01f766ea2dba07ee309e
Alfonso Manjarrez Guido <poncho_997@uabc.edu.mx>
2020-05-26 23:04:40 -0700

    Added respective Model and Views for IAD system

--- Controllers/DocenteController.cs
+++ Controllers/DocenteController.cs
@@ -14,6 +14,7 @@ using ISProject.Filters;
 
 namespace ISProject.Controllers
 {
+    #region Metodos del PAAD
     [FilterDocente]
     public class DocenteController : Controller
     {
@@ -22,6 +23,8 @@ namespace ISProject.Controllers
         {
             return View("HomeDocente");
         }
+        #region Metodos del PAAD
+
         //ModifyPAAD actions
         public ActionResult ModifyPAAD()
         {
@@ -240,7 +243,8 @@ namespace ISProject.Controllers
             List<RegistroPAAD> list = GetRecordPAADs(Convert.ToInt32(id_period));
             return PartialView("_ListPAADs", list);
         }
-        //Utility functions
+        #endregion
+        #region Utility functions
         public InfoPAADCLS GetInfoPAAD(int id=0)
         {
             InfoPAADCLS info = new InfoPAADCLS();
@@ -279,6 +283,7 @@ namespace ISProject.Controllers
             }
             return info;
         }
+
         public HeaderPAADCLS GetHeader(int id)
         {
             HeaderPAADCLS header = null;
@@ -322,6 +327,7 @@ namespace ISProject.Controllers
             }
             return header;
         }
+
         public List<ActivityCLS> GetActivities(int id)
         {
             List<ActivityCLS> activities = null;
@@ -345,6 +351,7 @@ namespace ISProject.Controllers
             }
             return activities;
         }
+
         public List<RegistroPAAD> GetRecordPAADs(int period=0)
         {
             List<RegistroPAAD> list = null;
@@ -375,6 +382,7 @@ namespace ISProject.Controllers
             }
             return list;
         }
+
         public List<SelectListItem> GetPeriods()
         {
             List<SelectListItem> periods=null;
@@ -390,6 +398,7 @@ namespace ISProject.Controllers
             }
             return periods;
         }
+
         public MessagesPAADCLS GetMessages(int id)
         {
             MessagesPAADCLS msg;
@@ -420,5 +429,227 @@ namespace ISProject.Controllers
                 return sw.GetStringBuilder().ToString();
             }
         }
+        #endregion
+        #region Metodos del IAD
+
+        //ModifyIAD actions
+        public ActionResult ModifyIAD()
+        {
+            InfoPAADCLS info = GetInfoPAAD();
+            if (info != null && info.status_value != 1)
+                return RedirectToAction("ViewPAAD", new { id = info.id_paad });
+            ViewBag.info = info;
+            ViewBag.header = GetHeader(info.id_paad);
+            ViewBag.activities = GetActivities(info.id_paad);
+            ViewBag.msg = GetMessages(info.id_paad);
+            return View();
+        }
+        public ActionResult ModalActivity_IAD(int mdl_id_iad, int mdl_id_activity)
+        {
+            ActivityCLS modal = null;
+            if (mdl_id_activity > 0)
+            {
+                using (var db = new DB_PAAD_IADEntities())
+                {
+                    modal = (from activity in db.Actividades
+                             where activity.id_actividad == mdl_id_activity
+                             select new ActivityCLS
+                             {
+                                 id = activity.id_actividad,
+                                 actividad = activity.actividad,
+                                 produccion = activity.produccion,
+                                 lugar = activity.lugar,
+                                 porcentaje_inicial = activity.porcentaje_inicial,
+                                 porcentaje_final = activity.porcentaje_final,
+                                 cacei = activity.cacei,
+                                 cuerpo_academico = activity.cuerpo_academico,
+                                 iso = activity.iso,
+                                 id_paad = activity.id_paad
+                             }).FirstOrDefault();
+                }
+            }
+            else
+            {
+                modal = new ActivityCLS { id_paad = mdl_id_iad };
+            }
+            return PartialView("_ModalActivityPAAD", modal);
+        }
+        [HttpPost]
+        public ActionResult SaveActivity_IAD(ActivityCLS model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Json(new
+                {
+                    Status = 2,
+                    Message = "Invalid",
+                    AjaxResponse = RenderRazorViewToString("_ModalActivityIAD", model)
+                });
+            }
+            else
+            {
+                if (model.id != 0)
+                {
+                    using (var db = new DB_PAAD_IADEntities())
+                    {
+                        Actividades act_db = db.Actividades.Single(p => p.id_actividad == model.id);
+                        act_db.actividad = model.actividad;
+                        act_db.actividad = model.actividad;
+                        act_db.produccion = model.produccion;
+                        act_db.lugar = model.lugar;
+                        act_db.porcentaje_inicial = model.porcentaje_inicial;
+                        act_db.cacei = model.cacei;
+                        act_db.cuerpo_academico = model.cuerpo_academico;
+                        act_db.iso = model.iso;
+                        db.SaveChanges();
+                    }
+                }
+                else
+                {
+                    using (var db = new DB_PAAD_IADEntities())
+                    {
+                        db.Actividades.Add(new Actividades
+                        {
+                            actividad = model.actividad,
+                            produccion = model.produccion,
+                            lugar = model.lugar,
+                            porcentaje_inicial = model.porcentaje_inicial,
+                            cacei = model.cacei,
+                            cuerpo_academico = model.cuerpo_academico,
+                            iso = model.iso,
+                            id_paad = model.id_paad
+                        });
+                        db.SaveChanges();
+                    }
+                }
+                List<ActivityCLS> list = GetActivities(model.id_paad);
+                return Json(new
+                {
+                    Status = 1,
+                    Message = "Success",
+                    AjaxResponse = RenderRazorViewToString("_EditActivitiesTable", list)
+                });
+            }
+        }
+        public ActionResult DeleteActivity_IAD(int del_id_paad, int del_id_activity)
+        {
+            using (var db = new DB_PAAD_IADEntities())
+            {
+                Actividades act_db = db.Actividades.Single(p => p.id_actividad == del_id_activity);
+                db.Actividades.Remove(act_db);
+                db.SaveChanges();
+            }
+            List<ActivityCLS> list = GetActivities(del_id_paad);
+            return PartialView("_EditActivitiesTable", list);
+        }
+        public ActionResult DropActivities_IAD(int drop_id_paad)
+        {
+            using (var db = new DB_PAAD_IADEntities())
+            {
+                List<Actividades> act_db = db.Actividades.Where(p => p.id_paad == drop_id_paad).ToList();
+                foreach (var item in act_db)
+                {
+                    db.Actividades.Remove(item);
+                }
+                db.SaveChanges();
+            }
+            return PartialView("_EditActivitiesTable", new List<ActivityCLS>());
+        }
+        public void ChangeCargo_IAD(string id_cargo, int id_paad)
+        {
+            using (var db = new DB_PAAD_IADEntities())
+            {
+                PAADs doc_paad = (from paad in db.PAADs where paad.id_paad == id_paad select paad).FirstOrDefault();
+                doc_paad.cargo = Convert.ToInt32(id_cargo);
+                db.SaveChanges();
+            }
+        }
+        //ViewIAD_Docente Actions
+        public ActionResult ViewIAD(int id)
+        {
+            InfoPAADCLS info = GetInfoPAAD(id);
+            if (info == null && info.status_value == 1)
+                return RedirectToAction("ModifyPAAD");
+            ViewBag.info = info;
+            ViewBag.header = GetHeader(info.id_paad);
+            ViewBag.activities = GetActivities(info.id_paad);
+            ViewBag.msg = GetMessages(info.id_paad);
+            return View("ViewPAAD_Docente");
+        }
+        public ActionResult ApplyActionIAD(AuthenticationCLS credentials, int id_paad, int action_paad, string message_modif = null)
+        {
+            if (!ModelState.IsValid)
+                return Json(new
+                {
+                    Status = 2,
+                    Message = "Invalid",
+                    AjaxResponse = RenderRazorViewToString("_AuthenticateCredentials", credentials)
+                });
+            Docentes doc = ((Docentes)Session["user"]);
+            if (!auth.AuthenticateCredentials(credentials.email, credentials.password) || doc.correo != credentials.email)
+            {
+                credentials.message = "Correo y/o contraseña incorrectos";
+                return Json(new
+                {
+                    Status = 3,
+                    Message = "Error",
+                    AjaxResponse = RenderRazorViewToString("_AuthenticateCredentials", credentials)
+                });
+            }
+            using (var db = new DB_PAAD_IADEntities())
+            {
+                PAADs paad = db.PAADs.Where(p => p.id_paad == id_paad && p.docente == doc.id_docente).FirstOrDefault();
+                if (paad == null)
+                {
+                    credentials.message = "Correo y/o contraseña incorrectos";
+                    return Json(new
+                    {
+                        Status = 3,
+                        Message = "Error",
+                        AjaxResponse = RenderRazorViewToString("_AuthenticateCredentials", credentials)
+                    });
+                }
+                if (action_paad == 1)
+                {
+                    paad.estado = 2;
+                    paad.razones_rechazo = null;
+                    paad.firma_docente = Guid.NewGuid().ToString("N");
+                }
+                else if (action_paad == 2)
+                {
+                    paad.estado = 1;
+                    paad.firma_docente = null;
+                }
+                else if (action_paad == 3)
+                {
+                    paad.estado = 4;
+                    paad.razones_modificacion = message_modif;
+                    paad.razones_rechazo_solicitud = null;
+                }
+                else if (action_paad == 4)
+                {
+                    paad.estado = 3;
+                }
+                db.SaveChanges();
+            }
+            return Json(new
+            {
+                Status = 1,
+                Message = "Success"
+            });
+        }
+        //ListRecordIADs_Docente Actions
+        public ActionResult ListRecordPAADs()
+        {
+            ViewBag.list = GetRecordPAADs();
+            ViewBag.periods = GetPeriods();
+            return View("ListRecordPAADs_Docente");
+        }
+        public ActionResult FilterRecordPAADs(string id_period)
+        {
+            List<RegistroPAAD> list = GetRecordPAADs(Convert.ToInt32(id_period));
+            return PartialView("_ListPAADs", list);
+        }
+        #endregion
     }
 }
